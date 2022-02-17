d3385292d5bcc2c57a5065820995214f8ff44c79
Bruno Pinho <brunopinho@outlook.com>
2022-02-17 08:45:25 -0300

    feat: created style for footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,9 @@
 import React from "react";
+import './Footer.css';
 
 const Footer = () => {
   return (
-    <p>Criado por <strong><a href="https://github.com/pinhob" target='_blank' rel='noreferrer'>Bruno Pinho</a></strong> para a <strong>Ebytr</strong> </p>
+    <p>Criado por <a href="https://github.com/pinhob" target='_blank' rel='noreferrer' className="profile">Bruno Pinho</a> para a <strong>Ebytr</strong> </p>
   )
 }
 
